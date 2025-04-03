bda6fe6e4fd35c2775f884f9518ec90d31be5b81
nvnsudharsan <nvnsuhdarsan@gmail.com>
2025-04-03 12:19:05 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # SOLWEIG-GPU
+
+A high-performance GPU-accelerated pipeline for computing the Universal Thermal Climate Index (UTCI), tested with NVIDIA Modulus 23.01.
+
+## Installation
+
+```bash
+pip install .
+```
+
+## Requirements
+
+- Python 3.8+
+- Tested with Modulus 23.01 (CUDA 12+)
+- GPU with sufficient memory
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="solweig-gpu",
+    version="0.1.0",
+    description="GPU-accelerated SOLWEIG model for urban thermal comfort simulation",
+    author="Harsh Kamath, Naveen Sudharsan",
+    author_email="harsh.kamath@utexas.edu, naveens@utexas.edu",
+    url="https://github.com/nvnsudharsan/utci-pipeline",
+    packages=find_packages(),  # Automatically finds `solweig_gpu`
+    install_requires=[
+        "torch",
+        "numpy",
+        "scipy",
+        "pandas",
+        "netCDF4",
+        "pytz",
+        "shapely",
+        "timezonefinder",
+        "gdal"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+    ],
+    python_requires=">=3.8",
+    entry_points={
+        'console_scripts': [
+            'thermal_comfort=solweig_gpu.cli:main',
+        ],
+    },
+)
