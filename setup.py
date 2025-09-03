f63358eddb6785a76ff9fe547eb126e353a1139f
Naveen Sudharsan <57706774+nvnsudharsan@users.noreply.github.com>
2025-09-03 16:14:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="solweig-gpu",
-    version="1.2.14",
+    version="1.2.15",
     description="GPU-accelerated SOLWEIG model for urban thermal comfort simulation",
     long_description=long_description,
     long_description_content_type='text/markdown',
