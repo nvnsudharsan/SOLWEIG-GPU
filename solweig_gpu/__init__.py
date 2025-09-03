70383c9c7a0120e169da2b61643cebc49d2d3f4d
Naveen Sudharsan <nvnsudharsan@gmail.com>
2025-09-03 13:03:53 -0500

    Update __init__.py

--- solweig_gpu/__init__.py
+++ solweig_gpu/__init__.py
@@ -1,2 +1,7 @@
 __version__ = "1.2.14"
 from .solweig_gpu import thermal_comfort
+import klyne
+klyne.init(
+    api_key="klyne_4vbZoO6NHl5W88il1jfnPxxEJ-lbXefZLP0RnC1DjqI",
+    project="solweig-gpu"
+)
