26f9a4ebbcf25831c17399e494fcd209316e0104
HarshKamath <55356169+HarshKamath@users.noreply.github.com>
2026-03-19 22:10:31 -0500

    Numpy upgrade proof

--- solweig_gpu/sun_position.py
+++ solweig_gpu/sun_position.py
@@ -960,8 +960,8 @@ def Solweig_2015a_metdata_noload(inputdata, location, UTC):
             fifteen = 0.
             sunmaximum = -90.
             sunmax['zenith'] = 90.
-            while sunmaximum <= 90. - sunmax['zenith']:
-                sunmaximum = 90. - sunmax['zenith']
+            while sunmaximum <= 90. - float(np.asarray(sunmax['zenith']).squeeze()):
+                sunmaximum = 90. - float(np.asarray(sunmax['zenith']).squeeze())
                 fifteen = fifteen + 15. / 1440.
                 HM = datetime.timedelta(days=(60*10)/1440.0 + fifteen)
                 YMDHM = YMD + HM
@@ -984,11 +984,13 @@ def Solweig_2015a_metdata_noload(inputdata, location, UTC):
         time['hour'] = YMDHM.hour
         time['min'] = YMDHM.minute
         sun = sun_position(time, location)
-        if (sun['zenith'] > 89.0) & (sun['zenith'] <= 90.0):    
-            sun['zenith'] = 89.0
-        altitude[0, i] = 90. - sun['zenith']
-        zen[0, i] = sun['zenith'] * (np.pi/180.)
-        azimuth[0, i] = sun['azimuth']
+        sun_zenith = float(np.asarray(sun['zenith']).squeeze())
+        sun_azimuth = float(np.asarray(sun['azimuth']).squeeze())
+        if (sun_zenith > 89.0) and (sun_zenith <= 90.0):
+            sun_zenith = 89.0
+        altitude[0, i] = 90. - sun_zenith
+        zen[0, i] = sun_zenith * (np.pi / 180.)
+        azimuth[0, i] = sun_azimuth
 
         # day of year and check for leap year
         if calendar.isleap(time['year']):
